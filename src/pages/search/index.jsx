9926fb1d396d1850e4b413d30f565c0b883bc34e
Cassie Andrews <candrews1@ufl.edu>
2025-06-08 15:41:08 -0400

    implemented most functionality, final troubleshooting

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,36 +1,76 @@
 import BookPreview from "../../components/bookPreview";
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import styles from './style.module.css'
 
 export default function Search() {
   // stores search results
-  const [bookSearchResults, setBookSearchResults] = useState()
+  const [bookSearchResults, setBookSearchResults] = useState([])
   // stores value of input field
   const [query, setQuery] = useState("")
   // compare to query to prevent repeat API calls
-  const [previousQuery, setPreviousQuery] = useState()
+  const [previousQuery, setPreviousQuery] = useState("")
   // used to prevent rage clicks on form submits
   const [fetching, setFetching] = useState(false)
+  
+  const inputRef = useRef()
+  const inputDivRef = useRef()
 
   // TODO: When the Search Page loads, use useEffect to fetch data from:
   // https://www.googleapis.com/books/v1/volumes?langRestrict=en&maxResults=16&q=YOUR_QUERY
   // Use a query of "React"
+  useEffect(() => {
+    async function getBookSearchResults(query) {
+      setFetching(true)
+      try {
+        const res = await fetch(`https://www.googleapis.com/books/v1/volumes?langRestrict=en&maxResults=16&q=${query}`)
+        const data = await res.json()
+        setBookSearchResults(data.items || [])
+        setPreviousQuery(query)
+        setQuery(query)
+        setFetching(false)
+      } catch {
+        console.log("error")
+        setFetching(false)
+      }
+    }
+    
+    getBookSearchResults("React")
+  }, [])
+
 
   // TODO: Write a submit handler for the form that fetches data from:
   // https://www.googleapis.com/books/v1/volumes?langRestrict=en&maxResults=16&q=YOUR_QUERY
-  // and stores the "items" property in the result to the bookSearchResults variable
-  // This function MUST prevent repeat searches if:
-  // fetch has not finished
-  // the query is unchanged
+  async function handleSubmit(e) {
+    e.preventDefault()
 
-  const inputRef = useRef()
-  const inputDivRef = useRef()
+    const newQuery = query
+
+    if (fetching || newQuery === previousQuery) return
+    // This function MUST prevent repeat searches if:
+    // fetch has not finished
+    // the query is unchanged
+    setFetching(true)
+
+    try {
+      const res = await fetch(`https://www.googleapis.com/books/v1/volumes?langRestrict=en&maxResults=16&q=${query}`)
+      const data = await res.json()
+      // and stores the "items" property in the result to the bookSearchResults variable
+      setBookSearchResults(data.items || [])
+      setPreviousQuery(query)
+      setFetching(false)
+    } catch {
+      console.log("error")
+      setBookSearchResults("React")
+      setFetching(false)
+    }
+  }
+
 
   return (
     <main className={styles.search}>
       <h1>Book Search</h1>
       {/* TODO: add an onSubmit handler */}
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <label htmlFor="book-search">Search by author, title, and/or keywords:</label>
         <div ref={inputDivRef}>
           {/* TODO: add value and onChange props to the input element based on query/setQuery */}
@@ -39,6 +79,8 @@ export default function Search() {
             type="text"
             name="book-search"
             id="book-search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
             />
           <button type="submit">Submit</button>
         </div>
@@ -51,7 +93,18 @@ export default function Search() {
         ? <Loading />
         : bookSearchResults?.length
         ? <div className={styles.bookList}>
-            {/* TODO: render BookPreview components for each search result here based on bookSearchResults */}
+            {bookSearchResults.map(book => {
+              const { title, authors, imageLinks, previewLink } = book.volumeInfo 
+              return (
+              <BookPreview 
+                key={book.id} 
+                title={title}
+                authors={authors}
+                thumbnail={imageLinks?.thumbnail}previewLink={previewLink} 
+              /> 
+              /* TODO: render BookPreview components for each search result here based on bookSearchResults */
+            )
+            })}
           </div>
         : <NoResults
           {...{inputRef, inputDivRef, previousQuery}}
